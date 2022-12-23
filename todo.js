2a701337faf52c4c607102ef6389a85b35829170
Shreejal Maharjan <shreejal27@gmail.com>
2022-12-23 12:05:22 +0545

    stored user input in the div

--- todo.js
+++ todo.js
@@ -1,8 +1,27 @@
+const form = document.getElementById("form");
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  var con= confirm ("Do You Want To Add?");
+  if (con == true){
+    
+      var msg = document.getElementById("message").value;
+
+     document.getElementById("text1").innerHTML = msg;
+  
+  }
+  else{
+      return false;
+  }
+});
+
+
+
 
 function cross(){
     var checkBox= document.getElementById("check");
 
-    var text = document.getElementById("text1");
+    
 
     if(checkBox.checked == true){
         text.style.textDecoration = "line-through";
@@ -12,3 +31,15 @@ function cross(){
     }
   
 }
+
+function addTask(){
+    var con= confirm ("Do You Want To Add?");
+    if (con == true){
+        
+
+    }
+    else{
+        return false;
+    }
+
+    }
